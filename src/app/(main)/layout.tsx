ffa12ef1541d1ec37b027a8415b5330e9cae4212
KETAM ROSHAN SAI <156491345+RoshansaiK@users.noreply.github.com>
2025-06-07 15:18:59 +0530

    Update layout.tsx

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -12,7 +12,7 @@ export default function MainAppLayout({
       <footer className="py-6 md:px-8 md:py-0">
         <div className="container flex flex-col items-center justify-between gap-4 md:h-20 md:flex-row">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Built by Your Name. The source code is available on GitHub.
+            Built by Roshansaiketam. The source code is available on GitHub.
           </p>
         </div>
       </footer>
